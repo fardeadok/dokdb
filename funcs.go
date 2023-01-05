e72f0592597a805d4ba41de5ce202c952c6885a7
fardeadok <fardeadok@yandex.ru>
2023-01-05 16:04:49 +0500

    1601

--- funcs.go
+++ funcs.go
@@ -1,5 +1,7 @@
 package dokdb
 
+//	--------------------
+//
 // return min, then max
 func sortX(x1, x2 float64) (minX, maxX float64) {
 	if x1 < x2 {
@@ -19,20 +21,26 @@ func checkBetweenX(minX, maxX, x float64) bool {
 	return false
 }
 
+//	--------------------
+//
+// make new point from lat, long
 func MakePoint(x, y float64) coords {
 	return coords{Lat: x, Long: y}
 }
 
+//	--------------------
+//
+// check point in rect
 func checkPointInRect(p1, p2 coords, p coords) bool {
 	minx, maxx := sortX(p1.Lat, p2.Lat)
 	betweenX := checkBetweenX(minx, maxx, p.Lat)
-	if betweenX == false {
+	if !betweenX {
 		return false
 	}
 
 	miny, maxy := sortX(p1.Long, p2.Long)
 	betweenY := checkBetweenX(miny, maxy, p.Long)
-	if betweenY == false {
+	if !betweenY {
 		return false
 	}
 
